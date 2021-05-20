ad9e9e6d5f82387010127010a274460bb46c46f7
MayKama <62993571+MayKama@users.noreply.github.com>
2021-05-20 18:00:17 +0100

    Add files via upload

---
+++ temperatureconversion.js
@@ -0,0 +1,23 @@
+function convertFahrToCelsius(fahrTemp){
+
+// check if fahrTemp cannot be converted to a valid number
+if ( isNaN(fahrTemp) ){ //fahrTemp is not a valid number
+    return `${JSON.stringify(fahrTemp)} is not a valid number but a/an ${ Array.isArray( fahrTemp) ? "array" : typeof(fahrTemp)}.`
+
+     }
+else {// fahrTemp is a valid number
+    let c =  ( Number(fahrTemp) - 32) * ( 5  / 9);
+    return c.toFixed(4); 
+}
+
+}
+
+convertFahrToCelsius(0)
+
+convertFahrToCelsius("0")
+
+convertFahrToCelsius([1,2,3])
+
+document.write(convertFahrToCelsius({temp: 0}))
+
+
